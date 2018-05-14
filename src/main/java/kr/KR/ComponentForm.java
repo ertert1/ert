cfe5939c32a81fe2638f69947a0af6f1dcc90623
Amur21 <34628045+Amur21@users.noreply.github.com>
2018-05-14 19:33:00 +0500

    Update ComponentForm.java

--- src/main/java/kr/KR/ComponentForm.java
+++ src/main/java/kr/KR/ComponentForm.java
@@ -30,3 +30,36 @@ public class ComponentForm extends JFrame {
 	static JLabel label_2;
 	static JLabel label_3;
 	static JTextField TxtBox1;
+	static JTextField TxtBox2;
+	public static JFrame frame;
+	static JButton btnNewButton;
+	/**
+	 * Объявление метода, в котором объявляются элементы экранной формы
+	 * SuppressWarnings-это объявление аннотации для отключения предупреждений об ошибках
+	 * Значок собаки может использоваться и для вызова индентифицирующих методов, для инициализации потоковых ресурсов
+	 */
+	@SuppressWarnings("deprecation")
+	ComponentForm() {
+		/**
+		 * Задание шрифта и размера надписей для кнопок, меток;
+		 */
+		Font font = new Font("Times New Roman", Font.PLAIN, 20);
+		/**
+		 * Создание и настройка компонентов формы
+		 */
+		JPanel Panel1 = new JPanel();
+		TxtBox1=new JTextField("");
+		TxtBox1.setFont(font);
+		TxtBox1.setPreferredSize( new Dimension( 100, 24 ) );
+		TxtBox2=new JTextField();
+		TxtBox2.setFont(font);
+		TxtBox2.setPreferredSize( new Dimension( 100, 24 ) );
+		label_1=new JLabel("Максимальная мощность, Вт");
+		label_1.setFont(font);
+		label_2=new JLabel("Напряжение сети, В");
+		label_2.setFont(font);
+		label_3=new JLabel(" ");
+		label_3.setFont(font);
+		btnNewButton=new JButton("Расчитать");
+		btnNewButton.setFont(font);
+		Panel1 = new JPanel();
