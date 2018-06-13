1d6d4ea29f93a265047a6fc62ff2f2762d13eceb
ertert1 <34750986+ertert1@users.noreply.github.com>
2018-06-13 22:34:13 +0500

    Update CalcTest3.java

--- src/test/java/kr/KR/CalcTest3.java
+++ src/test/java/kr/KR/CalcTest3.java
@@ -26,6 +26,6 @@ public class CalcTest3{
 			/**
 			   * Проверка того установлен ли шрифт для кнопки
 			   */
-		  assertTrue(ComponentForm.btnNewButton.isFontSet()==false);
+		  assertTrue(ComponentForm.btnNewButton.isFontSet());
 	}
 }
